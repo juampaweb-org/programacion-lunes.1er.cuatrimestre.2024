be068b5fb286c763a640a3f17373b5e84226d5f7
Profe Juan <juanpablososa@gmail.com>
2024-04-28 14:05:07 -0300

    unidad 06 - Ejercicios de funciones

---
+++ unidad-06/ejercicios/script-08.py
@@ -0,0 +1,3 @@
+
+# Cálculo de la hipotenusa: Escribe una función que calcule la longitud de la hipotenusa de un triángulo rectángulo dado sus dos catetos.
+
---
+++ unidad-06/ejercicios/script-09.py
@@ -0,0 +1,1 @@
+# Cálculo del área de un círculo: Define una función que calcule el área de un círculo dado su radio.
