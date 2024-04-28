d9569ca2d1f9c2b78b034e61954227a1f1c78f61
Profe Juan <juanpablososa@gmail.com>
2024-04-28 14:08:59 -0300

    Unidad 06 - Modifico algunos ejercicios

--- unidad-06/ejercicios/script-08.py
+++ unidad-06/ejercicios/script-08.py
@@ -1,3 +1,4 @@
 
-# Cálculo de la hipotenusa: Escribe una función que calcule la longitud de la hipotenusa de un triángulo rectángulo dado sus dos catetos.
+# Generar una función en donde mediante clave valor le pasemos datos de un alumno. Y segun lo que haya pasado imprime una ficha, en caso de que no haya pasado algun valor va a decir sin valor.. Debe poder pasar nombre, apellido, carrera, fecha_nacimiento, email, turno.
+
 
--- unidad-06/ejercicios/script-09.py
+++ unidad-06/ejercicios/script-09.py
@@ -1,1 +1,3 @@
-# Cálculo del área de un círculo: Define una función que calcule el área de un círculo dado su radio.
+# Realizar una función que reciba n parametros, el primero solamente puede ser o 'sumar' o 'multiplicar', y los siguientes deben ser numeros.
+# Si alguno no es un número debe avisar y cortar la ejecución. Y si verifica y son todos los números, debe devolver la suma o multiplicación de los números.
+
